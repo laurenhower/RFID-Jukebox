219ba13d23e95308df887d252f1abba33bd232e5
laurenhower <104145408+laurenhower@users.noreply.github.com>
2022-10-30 14:46:02 -0500

    Update Read.py

--- Read.py
+++ Read.py
@@ -8,7 +8,7 @@ reader = SimpleMFRC522()
 try:
         print("Waiting for you to scan an RFID sticker/card")
         id, text = reader.read()
-        print("The texr for this card is:", text.strip())
+        print("The text for this card is:", text.strip())
         
 finally:
         GPIO.cleanup()
